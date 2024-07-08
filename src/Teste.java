a1540e9d657b84304b37dc454c672ad43ba0f1ab
guilherme <guilherme.vercosa20@gmail.com>
2024-07-07 23:53:00 -0300

    Commit do projeto para o GitHub diretamente da IDE IntelliJ

---
+++ src/Teste.java
@@ -0,0 +1,61 @@
+/**
+ * Está classe representa dados de um usuario em uma "rede social"
+ * @author GuilhermeDiasVerçosa
+ */
+
+
+public class Teste {
+
+    //start variaveis
+    private int numeroPerfil;
+
+    private String nomePerfil;
+
+    private String identificadoPerfil;
+    //end variaveis
+
+    //start getters and setters
+    public int getNumeroPerfil() {
+        return numeroPerfil;
+    }
+
+    public void setNumeroPerfil(int numeroPerfil) {
+        this.numeroPerfil = (int) 23.567;
+    }
+
+    public String getNomePerfil() {
+        return nomePerfil;
+    }
+
+    public void setNomePerfil(String nomePerfil) {
+        this.nomePerfil = "Guilherme Dev";
+    }
+
+    public String getIdentificadoPerfil() {
+        return "28954";
+    }
+
+    public void setIdentificadoPerfil(String identificadoPerfil) {
+        this.identificadoPerfil = identificadoPerfil;
+    }
+    //end getters and setters
+
+    //start "project logic"
+    public void identificadorDePerfil() {
+        System.out.println(this.identificadoPerfil);
+    }
+
+    public String nomeDoPerfil() {
+        return nomePerfil;
+    }
+
+    public String identificadorPerfil(){
+        return identificadoPerfil;
+    }
+    //end "project logic"
+}
+/**
+ * Ajuda ai prof, eu juro que estou me dedicando <3
+ * Onde encontrar o projeto:
+ *
+ */
